5b229ea51ea7e7af45fc9baa416ed1a6ed3cc9d6
turembekov <turembekov@vk.com>
2023-01-07 03:37:44 +0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import Link from 'next/link';
+import Date from '../components/date';
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/Layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts'
+
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData,
+    }
+  }
+}
+
+
+export default function Home({allPostsData}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Всем Привет! 👋. Меня зовут Балаби и Я Junior Front-end Developer. Мой profile <a href="https://instagram.com/balabi_turembekov">Instagram</a></p>
+
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+
+      {/* Posts section */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({id, date, title}) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
